b60bc0cb7ab57a0477ecc7cd41fb8ca1057612a4
dollars <xbarthel@gmail.com>
2015-06-02 18:59:18 +0200

    jobs preparation

---
+++ benchmarks/test_tabu_job.bash
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#PBS -l walltime=6:00:00
+#PBS -o test_tabu.out
+#PBS -e test_tabu.err
+#PBS -N test_tabu.py
+
+module load graph-tool/2.2.42-foss-2015a-Python-2.7.9
+
+# Go in the submission directory where the R script is located:
+PYTHONPATH=$PYTHONPATH:$HOME/packing_coloring_app/
+cd $HOME/packing_coloring_app/benchmarks
+
+python test_tabu.py
+
